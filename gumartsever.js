765b0a179033c5ef0e9ac163ca8d77cac64615b0
Pocodeonline <n20and@gmail.com>
2024-09-03 15:37:58 +0700

    gumartsever.js

--- gumartsever.js
+++ gumartsever.js
@@ -98,7 +98,7 @@ async function processAccount(browserContext, accountUrl, accountNumber, proxy) 
         await page.goto(accountUrl);
 
         const pageLoadedSelector = '#__nuxt > div > div > div.fixed.bottom-0.w-full.left-0.z-\\[12\\] > div > div.grid.grid-cols-5.w-full.gap-2 > button:nth-child(3) > div > div.shadow_filter.w-\\[4rem\\].h-\\[4rem\\].absolute.-translate-y-\\[50\\%\\] > img';
-        await page.waitForSelector(pageLoadedSelector, { timeout: 6000 });
+        await page.waitForSelector(pageLoadedSelector, { timeout: 10000 });
         console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${GREEN}Đã vào giao diện ${await page.title()} Acc ${YELLOW}${accountNumber}`);
 
         const claimButtonSelector = '#__nuxt > div > div > section > div.relative.z-\\[2\\].px-2.flex.flex-col.gap-2 > div > div > div > div.transition-all > button';
@@ -196,7 +196,7 @@ async function runPlaywrightInstances(links, proxies, maxBrowsers) {
         }
 
         if (activeCount > 0) {
-            await new Promise(resolve => setTimeout(resolve, 10500));
+            await new Promise(resolve => setTimeout(resolve, 16000));
         }
     }
 
@@ -296,7 +296,7 @@ async function countdownTimer(seconds) {
 
             for (let i = 0; i <= repeatCount; i++) {
                 console.log(`${SILVER}Chạy lần ${GREEN}${i + 1}`);
-                await runPlaywrightInstances(links.slice(0, numAccounts), proxies, 6);
+                await runPlaywrightInstances(links.slice(0, numAccounts), proxies, 8);
 
                 if (i < repeatCount) {
                     await countdownTimer(restTime);
