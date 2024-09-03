d2cf083731cc1dfeb5eb5855419b349361d37c9c
Pocodeonline <n20and@gmail.com>
2024-09-03 22:12:32 +0700

    icebergsever.js

---
+++ icebergsever.js
@@ -0,0 +1,322 @@
+const { chromium } = require('playwright');
+const fs = require('fs');
+const readline = require('readline');
+
+const SILVER = '\x1b[38;5;231m';
+const LIGHT_PINK = '\x1b[38;5;207m';
+const PINK = '\x1b[38;5;13m';
+const YELLOW = '\x1b[38;5;11m';
+const GREEN = '\x1b[38;5;10m';
+const RED = '\x1b[38;5;9m';
+const LIGHT_BLUE = '\x1b[38;5;12m';
+const DARK_BLUE = '\x1b[38;5;19m';
+const RESET = '\x1b[0m';
+
+const ERROR_LOG_PATH = 'failed_accounts.txt';
+const PROXIES_FILE_PATH = 'proxies.txt';
+
+async function readProxies(filePath) {
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    const proxies = [];
+    for await (const line of rl) {
+        if (line.trim()) {
+            const parts = line.split(':');
+            if (parts.length === 4) {
+                const [ip, port, username, password] = parts;
+                proxies.push({ server: `${ip}:${port}`, username, password });
+            } else {
+                console.error(`Proxy format error: ${line}`);
+            }
+        }
+    }
+    return proxies;
+}
+
+async function countNonEmptyLines(filePath) {
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let count = 0;
+    for await (const line of rl) {
+        if (line.trim()) {
+            count++;
+        }
+    }
+    return count;
+}
+
+async function readAccounts(filePath) {
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    const links = [];
+    for await (const line of rl) {
+        if (line.trim()) links.push(line.trim());
+    }
+    return links;
+}
+
+async function printCustomLogo(blink = false) {
+    const logo = [
+        " 🛒🛒🛒    🛒       🛒  🛒      🛒      🛒🛒      🛒🛒🛒  🛒🛒🛒🛒🛒 ",
+        "🛒    🛒   🛒       🛒  🛒🛒  🛒🛒     🛒  🛒     🛒    🛒    🛒 ",
+        "🛒         🛒       🛒  🛒 🛒🛒 🛒    🛒🛒🛒🛒    🛒    🛒    🛒",
+        "🛒   🛒🛒  🛒       🛒  🛒  🛒  🛒   🛒      🛒   🛒🛒🛒🛒    🛒 ",
+        "🛒    🛒   🛒       🛒  🛒      🛒  🛒        🛒  🛒    🛒    🛒",
+        " 🛒🛒🛒     🛒🛒🛒🛒🛒  🛒      🛒 🛒          🛒 🛒     🛒   🛒",
+        "                                                                         ",
+        "chờ một lát..."
+    ];
+    console.clear();
+    for (let i = 0; i < 5; i++) {
+        if (blink) {
+            console.log('\x1b[5m\x1b[32m' + logo.join('\n') + '\x1b[0m');
+        } else {
+            console.log('\x1b[32m' + logo.join('\n'));
+        }
+        await new Promise(resolve => setTimeout(resolve, 300));
+        console.clear();
+        await new Promise(resolve => setTimeout(resolve, 300));
+    }
+}
+
+async function processAccount(browserContext, accountUrl, accountNumber, proxy) {
+    const page = await browserContext.newPage();
+    try {
+        console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${PINK}🐮 Đang chạy tài khoản ${YELLOW}${accountNumber} ${PINK}IP ${YELLOW}:${PINK}${proxy.server}`);
+        await page.goto(accountUrl);
+
+        const pageLoadedSelector = '#root > div > div.css-5bbctu > div > div.css-9c836o > img';
+        await page.waitForSelector(pageLoadedSelector, { timeout: 15000 });
+        console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Đã vào giao diện Iceberg Acc ${YELLOW}${accountNumber}`);
+
+        const balanceSelector = '#root > div > div.css-5bbctu > div > div.css-17b4s3y > div.css-1cnibcu > p.chakra-text.css-2iljf0';
+        const balanceElement = await page.waitForSelector(balanceSelector, { timeout: 6000 });
+        const balanceText = await balanceElement.evaluate(el => el.innerText);
+        console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;12mSố dư hiện tại acc \x1b[33m${accountNumber} \x1b[38;5;12m là \x1b[38;5;11m: \x1b[38;5;11m${balanceText}`);
+
+        const claimButtonSelector = '#root > div > div.css-5bbctu > div > div.css-17b4s3y > div.chakra-offset-slide > button > span > svg';
+
+        try {
+            await page.waitForSelector(claimButtonSelector, { timeout: 5000 });
+            const claimButton = await page.$(claimButtonSelector);
+            if (claimButton) {
+                await claimButton.click();
+                console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;12mĐã claim acc \x1b[33m${accountNumber}`);
+            } else {
+                console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;12m Acc \x1b[33m${accountNumber} \x1b[38;5;12mclaim rồi...`);
+            }
+        } catch (err) {
+        }
+
+        const startminingButtonSelector = '#root > div > div.css-5bbctu > div > div.css-17b4s3y > div.chakra-offset-slide > button > div';      
+
+        try {
+            await page.waitForSelector(startminingButtonSelector, { timeout: 5000 });
+            const claimButton = await page.$(startminingButtonSelector);
+            if (claimButton) {
+                await claimButton.click();
+                console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;12mĐã startmining acc \x1b[33m${accountNumber}`);
+            } else {
+                console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;12m Acc \x1b[33m${accountNumber} \x1b[38;5;12mstartmining rồi...`);
+            }
+        } catch (err) {
+        }
+
+        const pointsSelector = '#root > div > div.css-5bbctu > div > div.css-17b4s3y > div.css-1cnibcu > p.chakra-text.css-2iljf0';
+        const pointsElement = await page.waitForSelector(pointsSelector);
+        const points = await pointsElement.evaluate(el => el.innerText);
+        console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Số dư khi làm xong acc\x1b[38;5;11m: ${points}`);
+
+    } catch (e) {
+        console.log(`${RED}Tài khoản số ${accountNumber} gặp lỗi`);
+        await logFailedAccount(accountNumber, e.message);
+        return false;
+    } finally {
+        await page.close();
+    }
+    return true;
+}
+
+async function runPlaywrightInstances(links, proxies, maxBrowsers) {
+    let totalSuccessCount = 0;
+    let totalFailureCount = 0;
+    let proxyIndex = 0;
+    let activeCount = 0;
+
+    async function processAccountWithBrowser(accountUrl, accountNumber, proxy) {
+        const browser = await chromium.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                `--proxy-server=${proxy.server}`
+            ]
+        });
+
+        const browserContext = await browser.newContext({
+            httpCredentials: {
+                username: proxy.username,
+                password: proxy.password
+            }
+        });
+
+        let accountSuccess = false;
+        try {
+            accountSuccess = await processAccount(browserContext, accountUrl, accountNumber, proxy);
+            if (accountSuccess) totalSuccessCount++;
+            else totalFailureCount++;
+        } catch (error) {
+            totalFailureCount++;
+        } finally {
+            await browserContext.close();
+            await browser.close();
+        }
+    }
+
+    const accountQueue = [...links];
+    while (accountQueue.length > 0 || activeCount > 0) {
+        while (activeCount < maxBrowsers && accountQueue.length > 0) {
+            const accountUrl = accountQueue.shift();
+            const accountNumber = links.indexOf(accountUrl) + 1;
+            const proxy = proxies[proxyIndex % proxies.length];
+            proxyIndex++;
+
+            activeCount++;
+            processAccountWithBrowser(accountUrl, accountNumber, proxy)
+                .then(() => {
+                    activeCount--;
+                    console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${GREEN}Hoàn tất tài khoản ${accountNumber}`);
+                })
+                .catch(() => {
+                    activeCount--;
+                    console.log(`${RED}Tài khoản ${accountNumber} gặp lỗi`);
+                });
+        }
+
+        if (activeCount > 0) {
+            await new Promise(resolve => setTimeout(resolve, 25000));
+        }
+    }
+
+    console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${GREEN}Hoàn tất xử lý tất cả tài khoản \x1b[38;5;231mTool \x1b[38;5;11m[ \x1b[38;5;231mICEBERG \x1b[38;5;11m].`);
+    console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${SILVER}Tổng tài khoản thành công: ${YELLOW}${totalSuccessCount}`);
+    console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${SILVER}Tổng tài khoản lỗi: ${YELLOW}${totalFailureCount}`);
+}
+
+async function logFailedAccount(accountNumber, errorMessage) {
+    fs.appendFileSync(ERROR_LOG_PATH, `Tài khoản số ${accountNumber} gặp lỗi\n`);
+}
+
+async function countdownTimer(seconds) {
+    for (let i = seconds; i >= 0; i--) {
+        process.stdout.write(`\r${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${RED}Đang nghỉ ngơi còn lại ${YELLOW}${i} ${RED}giây`);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    console.log();
+}
+
+(async () => {
+    await printCustomLogo(true);
+    const filePath = 'iceberg.txt';
+
+    try {
+        const proxies = await readProxies(PROXIES_FILE_PATH);
+        if (proxies.length === 0) {
+            console.log(`${RED}Không tìm thấy proxy nào.`);
+            return;
+        }
+
+        while (true) {
+            const nonEmptyLines = await countNonEmptyLines(filePath);
+            if (nonEmptyLines === 0) {
+                console.log(`${RED}File không chứa tài khoản nào.`);
+                break;
+            }
+
+            const links = await readAccounts(filePath);
+            console.log(`${SILVER}ICEBERG ${LIGHT_PINK}code by ${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] ${RESET}`);
+            console.log(`${LIGHT_PINK}tele${YELLOW}: ${PINK}tphuc_0 ${RESET}`);
+            console.log(`${LIGHT_BLUE}Hiện tại bạn có ${YELLOW}${nonEmptyLines}${LIGHT_BLUE} tài khoản`);
+
+            const userInput = await new Promise(resolve => {
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+                rl.question(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Nhập số lượng tài khoản muốn 🐮 chạy ${YELLOW}(${LIGHT_BLUE}hoặc ${YELLOW}'all' ${LIGHT_BLUE}để chạy tất cả${YELLOW}, ${RED}0 ${LIGHT_BLUE}để thoát${YELLOW}): `, (answer) => {
+                    rl.close();
+                    resolve(answer.trim());
+                });
+            });
+
+            let numAccounts;
+            if (userInput.toLowerCase() === 'all') {
+                numAccounts = links.length;
+            } else if (!isNaN(userInput)) {
+                numAccounts = parseInt(userInput, 10);
+                if (numAccounts <= 0) {
+                    break;
+                }
+                if (numAccounts > links.length) {
+                    numAccounts = links.length;
+                }
+            } else {
+                console.log(`${RED}Nhập không hợp lệ!`);
+                continue;
+            }
+
+            const restTime = parseInt(await new Promise(resolve => {
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+                rl.question(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Nhập thời gian nghỉ ngơi sau khi 🐮 chạy xong tất cả các tài khoản ${YELLOW}(${LIGHT_BLUE}Khuyên ${YELLOW}9000 ${LIGHT_BLUE}nha${YELLOW}): `, (answer) => {
+                    rl.close();
+                    resolve(answer.trim());
+                });
+            }), 10);
+
+            const repeatCount = parseInt(await new Promise(resolve => {
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+                rl.question(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Nhập số lần lặp lại sau thời gian nghỉ ngơi ${YELLOW}(${LIGHT_BLUE}hoặc ${YELLOW}0 ${LIGHT_BLUE}để chạy một lần): `, (answer) => {
+                    rl.close();
+                    resolve(answer.trim());
+                });
+            }), 10);
+
+            if (isNaN(repeatCount) || repeatCount < 0) {
+                console.log(`${RED}Nhập không hợp lệ!`);
+                continue;
+            }
+
+            for (let i = 0; i <= repeatCount; i++) {
+                console.log(`${SILVER}Chạy lần ${GREEN}${i + 1}`);
+                await runPlaywrightInstances(links.slice(0, numAccounts), proxies, 8);
+
+                if (i < repeatCount) {
+                    await countdownTimer(restTime);
+                }
+            }
+
+            console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${GREEN}Đã hoàn tất tất cả các vòng lặp.`);
+        }
+    } catch (e) {
+        console.log(`Bảo trì do lỗi...}`);
+    }
+})();
