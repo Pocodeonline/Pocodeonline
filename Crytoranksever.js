8c41352a593371adb0320a6d7f284f215d9e7b7e
Pocodeonline <n20and@gmail.com>
2024-09-03 16:38:01 +0700

    Crytoranksever.js

--- Crytoranksever.js
+++ Crytoranksever.js
@@ -92,10 +92,14 @@ async function processAccount(browserContext, accountUrl, accountNumber, proxy) 
         console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• \x1b[38;5;207m🐮 Đang chạy tài khoản \x1b[38;5;11m${accountNumber} \x1b[38;5;12mIP \x1b[38;5;11m: \x1b[38;5;12m${proxy.server}`);
         await page.goto(accountUrl);
 
+        const pageLoadedSelector = '#root > div > div.fixed.left-0.top-0.z-\\[100\\].flex.h-full.w-full.flex-col.items-center.gap-6.bg-black.px-4.pb-10.pt-12 > div.flex.w-full.gap-4 > button.ease.h-11.w-full.rounded-\\[10px\\].px-3.font-semibold.transition-opacity.duration-150.active\\:opacity-\\[0\\.7\\].border.border-main-blue.text-main-blue.w-full';
+        await page.waitForSelector(pageLoadedSelector, { timeout: 20000 });
+        console.log(`${YELLOW}[ \x1b[38;5;231mWIT KOEI \x1b[38;5;11m] \x1b[38;5;207m• ${LIGHT_BLUE}Đã vào giao diện Crytorank acc ${YELLOW}${accountNumber}`);
+
         const skipButtonSelector = '#root > div > div.fixed.left-0.top-0.z-\\[100\\].flex.h-full.w-full.flex-col.items-center.gap-6.bg-black.px-4.pb-10.pt-12 > div.flex.w-full.gap-4 > button.ease.h-11.w-full.rounded-\\[10px\\].px-3.font-semibold.transition-opacity.duration-150.active\\:opacity-\\[0\\.7\\].border.border-main-blue.text-main-blue.w-full';
 
         try {
-            await page.waitForSelector(skipButtonSelector, { timeout: 15000 });
+            await page.waitForSelector(skipButtonSelector, { timeout: 5000 });
             const skipButton = await page.$(skipButtonSelector);
             if (skipButton) {
                 await skipButton.click();
@@ -105,7 +109,6 @@ async function processAccount(browserContext, accountUrl, accountNumber, proxy) 
                 console.log(`${RED}Không thấy skip \x1b[38;5;11m${accountNumber} `);
             }
         } catch (err) {
-            console.log(`${RED}Lỗi skip `);
         }
 
         // Continue with balance extraction
