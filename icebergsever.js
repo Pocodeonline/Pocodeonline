4c11fe71f8d269e4e9b9abe74de0a678e6fab493
Pocodeonline <n20and@gmail.com>
2024-09-04 13:05:48 +0700

    icebergsever.js

--- icebergsever.js
+++ icebergsever.js
@@ -166,6 +166,8 @@ async function runPlaywrightInstances(links, proxies, maxBrowsers) {
 
         const browserContext = await browser.newContext({
             httpCredentials: {
+                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+                storageState: null,
                 username: proxy.username,
                 password: proxy.password
             }
