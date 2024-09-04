aaedcf58dfc21a371b641da03d9fa3945e0c581f
Pocodeonline <n20and@gmail.com>
2024-09-04 10:40:28 +0700

    matchain.js

--- matchain.js
+++ matchain.js
@@ -240,10 +240,11 @@ async function runPlaywrightInstances(links, proxies, maxBrowsers) {
         });
 
         const browserContext = await browser.newContext({
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
             httpCredentials: {
+                storageState: null,
                 username: proxy.username,
                 password: proxy.password,
-                storageState: null
             }
         });
 
