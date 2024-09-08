c1c665c9a041e8e8e88bf33bc3ee6505e004cdf4
Pocodeonline <n20and@gmail.com>
2024-09-09 05:16:29 +0700

    gumartsv.js

--- gumartsv.js
+++ gumartsv.js
@@ -187,6 +187,7 @@ async function runPlaywrightInstances(links, proxies, maxBrowsers) {
 
         const browserContext = await browser.newContext({
             httpCredentials: {
+                userAgent: "Mozilla/5.0 (Linux; Android 10; Redmi 4A / 5A Build/QQ3A.200805.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36",
                 storageState: null,
                 username: proxy.username,
                 password: proxy.password
