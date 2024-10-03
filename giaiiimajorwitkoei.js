dd0c77b1331ebabf2bc272f5d01d424bf8e5c495
Pocodeonline <n20and@gmail.com>
2024-10-03 17:25:49 +0700

    giaiiimajorwitkoei.js

--- giaiiimajorwitkoei.js
+++ giaiiimajorwitkoei.js
@@ -2,10 +2,10 @@
   "date": "27/09/2024",
   "tasks": [
     {
-      "choice_1": 10,
+      "choice_1": 15,
       "choice_2": 4,
-      "choice_3": 5,
-      "choice_4": 1
+      "choice_3": 9,
+      "choice_4": 10
     }
   ]
 }
