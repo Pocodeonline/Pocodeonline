bd54d052b61f9ef9cf01a4f7ae7fc33de923b27e
Pocodeonline <n20and@gmail.com>
2024-10-06 15:03:00 +0700

    gumartsv.js

--- gumartsv.js
+++ gumartsv.js
@@ -225,6 +225,11 @@ async function runPlaywrightInstances(links, proxies, maxBrowsers) {
                 password: proxy.password
             },
             bypassCSP: true,
+            viewport: { width: 375, height: 667 },
+            deviceScaleFactor: 2,
+            isMobile: true,
+            hasTouch: true,
+            userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
         });
 
         let accountSuccess = false;
