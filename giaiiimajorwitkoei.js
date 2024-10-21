296e3cefa533c705d16cfd04120536e50fa1e356
Pocodeonline <n20and@gmail.com>
2024-10-21 23:21:17 +0700

    giaiiimajorwitkoei.js

--- giaiiimajorwitkoei.js
+++ giaiiimajorwitkoei.js
@@ -1,11 +1,11 @@
 {
-  "date": "20/10/2024",
+  "date": "21/10/2024",
   "tasks": [
     {
-      "choice_1": 4,
-      "choice_2": 6,
-      "choice_3": 11,
-      "choice_4": 1
+      "1": ("#root > div > div > div > div > div.min-page-content-height > div.flex.flex-col.items-center > div > div:nth-child(2) > div.w-\\[350px\\].h-\\[350px\\].p-2.grid.grid-cols-4.gap-2 > div:nth-child(2)),
+      "2": ("#root > div > div > div > div > div.min-page-content-height > div.flex.flex-col.items-center > div > div:nth-child(2) > div.w-\\[350px\\].h-\\[350px\\].p-2.grid.grid-cols-4.gap-2 > div:nth-child(10)),
+      "3": ("#root > div > div > div > div > div.min-page-content-height > div.flex.flex-col.items-center > div > div:nth-child(2) > div.w-\\[350px\\].h-\\[350px\\].p-2.grid.grid-cols-4.gap-2 > div:nth-child(14)),
+      "4": ("#root > div > div > div > div > div.min-page-content-height > div.flex.flex-col.items-center > div > div:nth-child(2) > div.w-\\[350px\\].h-\\[350px\\].p-2.grid.grid-cols-4.gap-2 > div:nth-child(13))
     }
   ]
 }
