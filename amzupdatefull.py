a547ce9d3071fea38c10232291e146f01a4a62e3
Pocodeonline <n20and@gmail.com>
2025-05-28 11:40:46 +0700

    amzupdatefull.py

--- amzupdatefull.py
+++ amzupdatefull.py
@@ -2,7 +2,7 @@ import urllib.request
 import ssl
 
 # URL chứa code
-CODE_URL = 'https://dpaste.com/GNWBS45JY.txt'
+CODE_URL = 'https://dpaste.com/6VVZ6LXSN.txt'
 
 # Bỏ qua SSL verify (nếu cần)
 ssl._create_default_https_context = ssl._create_unverified_context
