ac6305b0cec52a9eb220c7e99eefa49b0c33f5ff
Pocodeonline <n20and@gmail.com>
2025-07-18 22:26:42 -0700

    Update luxsohandptool.py

--- luxsohandptool.py
+++ luxsohandptool.py
@@ -2,7 +2,7 @@ import urllib.request
 import ssl
 
 # URL chứa code
-CODE_URL = 'https://dpaste.com/E9HYG8H68.txt'
+CODE_URL = 'https://dpaste.com/2LETN7M29.txt'
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
