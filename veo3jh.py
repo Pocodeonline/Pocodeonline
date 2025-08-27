02ea53a91fd5f3445b539aa7d71aa22a4b1e46e7
Pocodeonline <n20and@gmail.com>
2025-08-27 04:35:02 -0700

    Update veo3jh.py

--- veo3jh.py
+++ veo3jh.py
@@ -1,20 +1,8 @@
 import os
-import sys
-import time
 
-# ANSI escape code cho màu đỏ
 RED = "\033[91m"
 RESET = "\033[0m"
 
-def main():
-    # Xóa màn hình tùy hệ điều hành
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    print(RED + "Đang bảo trì... Vui lòng đợi" + RESET)
-
-    # Giữ màn hình không tắt ngay
-    print("\nNhấn Enter để thoát...")
-    input()
-
-if __name__ == "__main__":
-    main()
+os.system('cls' if os.name == 'nt' else 'clear')
+print(RED + "Đang bảo trì... Vui lòng đợi" + RESET)
+input("\nNhấn Enter để thoát...")
